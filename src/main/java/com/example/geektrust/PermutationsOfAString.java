4bd57e5ba7e672f38cb31a6f7dbcd93f80b30d6b
codersbuffet <chiranjeevi.ramanatha@gmail.com>
2022-01-03 08:44:30 +0530

    Permutations of a given string - without extra space using swap

---
+++ src/main/java/com/example/geektrust/PermutationsOfAString.java
@@ -0,0 +1,36 @@
+package com.example.geektrust;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+
+public class PermutationsOfAString {
+
+    public static void permute(int index, char[] array) {
+        if (index == array.length) {
+            for (char c : array) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        for (int i = index; i < array.length; i++) {
+            swap(i, index, array);
+            permute(index + 1, array);
+            swap(i, index, array);
+        }
+    }
+
+    public static void swap(int index1, int index2, char[] array) {
+        char temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "abc";
+        char[] charArray = input.toCharArray();
+        permute(0, charArray);
+
+    }
+}
